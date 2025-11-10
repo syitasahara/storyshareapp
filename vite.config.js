d5c88407630fd7114ac54ba3ce049800eebd7b92
syitasahara <masyitaika9@gmail.com>
2025-11-10 14:51:49 +0700

    Build for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/storyshareapp/',
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src', 'public'),
   build: {
